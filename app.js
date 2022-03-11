a0842fb1433b898aca9173e644da5212133efe00
sait <sait.bozteke63@gmail.com>
2022-03-11 02:40:14 +0100

    file added

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const searchProfile=document.querySelector("#searchProfile")
 
 
 searchProfile.addEventListener("keyup",(event)=>{
+    ui.clear()
     let text=event.target.value
     if(text!==""){
     profile.getProfile(text)
     .then(res=>{
         if(res.profile.length===0){
-
+          ui.showAlert(text)
         }else{
            ui.showProfile(res.profile[0])
         }
--- ui.js
+++ ui.js
@@ -3,6 +3,52 @@ class UI{
         this.profileContainer=document.querySelector("#profileContainer")
         this.alert=document.querySelector("#alert")
     }
-    
+    showProfile(profile){
+        this.profileContainer.innerHTML=`<div class="card card-body">
+        <div class="row">
+        <div class="col-md-3">
+             
 
+
+
+        <img src="https://via.placeholder.com/150.png/09f/fff" class="img-thumbnail">
+
+      
+
+        </div>
+        <div class="col-md-9">
+        <h4>Contact</h4>
+        <ul class="list-group">
+        <li class="list-group-item>
+        naem: ${profile.name}
+        </li>
+        <li class="list-group-item>
+        username: ${profile.username}
+        </li>
+        <li class="list-group-item>
+        adress: ${profile.address.street}
+                ${profile.address.city}
+                ${profile.address.zipcode}
+                ${profile.address.suite}
+        </li>
+        <li class="list-group-item>
+        website: ${profile.website}
+        </li>
+        <li class="list-group-item>
+        company: ${profile.company.name}
+        </li>
+        <li class="list-group-item>
+        phone: ${profile.phone}
+        </li>
+        </div>
+        </div>
+        </div>`
+    }
+    showAlert(text){
+this.alert.innerHTML=`${text} is not found`
+    }
+clear(){
+    this.profileContainer.innerHTML=""
+    this.alert.innerHTML=""
+}
 }
